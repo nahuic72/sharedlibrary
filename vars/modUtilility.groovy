6f0e03e3758e8152e892418fbe6710315609e79f
nahuic72 <45906252+nahuic72@users.noreply.github.com>
2022-05-05 14:24:50 +0200

    Delete modUtilility.groovy

--- vars/modUtilility.groovy
+++
@@ -1,7 +0,0 @@
-// vars/modUtility.groovy
-import groovy.json.JsonSlurperClassic
-
-Map parseJSONString(String json) {
-    def jsonSlurper = new JsonSlurperClassic()
-    return jsonSlurper.parseText(json) as Map
-}
